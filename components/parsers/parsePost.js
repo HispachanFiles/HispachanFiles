709bf5b00753718c018158ab206bd93d0117364e
HispachanFiles <hispachanfiles@gmail.com>
2016-08-05 11:59:37 -0400

    Fix WEBM

--- components/parsers/parsePost.js
+++ components/parsers/parsePost.js
@@ -75,7 +75,8 @@ function postMeta(post, $) {
         let fileMetaR = fileMeta.text().replace(/(\r\n|\n|\r|[()])/gm, "").split(',');
         let fileSize = fileMetaR[0].trim();
         let fileRes = (fileMetaR.length > 1) ? fileMetaR[1].trim() : '';
-        let fileON = (fileMetaR.length > 1) ? fileMetaR[2].trim() : fileInfo.find('a').first().attr('href').split('/').reverse()[0];
+        let fileON = (fileMetaR.length > 2) ? fileMetaR[2].trim() : fileInfo.find('a').first().attr('href').split('/').reverse()[0];
+        if (fileMetaR.length == 2) fileON = fileMetaR[1].trim();
         data.file = {
             url: fileInfo.find('a').first().attr('href'),
             size: fileSize,
